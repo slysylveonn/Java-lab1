0ebc0eccbeeabe0b97a74d4e9cb766e0f4e25ba9
slysylveonn <sophgelly@gmail.com>
2024-09-17 12:50:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const studentName = 'Sophie'
+var studentAge = 32
+var isEnrolled = true
+
+console.log (`Hello! My name is ${studentName}. I am ${studentAge} years old and I am currently enrolled at Humber College: ${isEnrolled}.`);
+
+let greeting = 'Welcome to my silly website!';
+let numOfDogs = 2;
+const aquarium = true;
+let aquariumFish = ['Guppy','Pleco','Gourami','Shrimp'];
+let dogInfo = {
+    age: 3,
+    name: 'Killua',
+    breed: 'Jindo'
+}
+
+console.log(`${greeting} I have ${numOfDogs} dogs. My favourite dog is ${dogInfo.name}. he is a ${dogInfo.breed} and he is ${dogInfo.age} years old.`);
+console.log(`I also have an aquarium: ${aquarium}. It is currently stocked with ${aquariumFish[0]}, ${aquariumFish[1]}, ${aquariumFish[2]}, ${aquariumFish[3]}.`);
+
+let mathSucks = 4;
+let mathRules = 16;
+
+mathRules /= mathSucks;
+mathSucks += mathRules;
+
+console.log(mathRules , mathSucks);
+console.log(mathRules > mathSucks);
+console.log(mathRules < mathSucks);
+
+if (mathRules||mathSucks===8);
+console.log('Math is the best!');
+
+if (mathRules&&mathSucks!==mathRules*mathSucks);
+console.log('Hooray for functions!');
+
+console.log(mathSucks-mathRules);
+
+
+
